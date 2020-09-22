c1e38ef0c4d737e9f3327b37faf50bc59066492d
DESKTOP-5MNNJOL\Mustafa <mustafa.saraoglu@mailbox.tu-dresden.de>
2020-09-23 00:52:24 +0200

    Path of GridMap added to the startup file.

--- addPath_Startup.m
+++ addPath_Startup.m
@@ -1,2 +1,4 @@
 % Add all the folders and subfolders to path (Project startup add to path doesn't work properly on other PCs)
 addpath(genpath(cd));
+
+addpath(genpath('.\src\Misc\NewGridVisualization'))
