986b8bf63b597638e5fa0da9bef787a48d927c94
Mustafa Saraoğlu <51496368+MustafaSaraoglu@users.noreply.github.com>
2020-11-17 17:17:53 +0100

    Reordering and recategorizing the folders for a better structure for Classes, Scripts, System Block Code, 3D Animation and Misc folders.

--- src/Scripts/Load Data/addPath_Startup.m
+++ src/Scripts/Load Data/addPath_Startup.m
@@ -1,4 +1,2 @@
 % Add all the folders and subfolders to path (Project startup add to path doesn't work properly on other PCs)
 addpath(genpath(cd));
-
-addpath(genpath('.\src\Misc\NewGridVisualization'))
