9df7812a050b9639746bea36f005184dabcf9001
Mustafa Saraoğlu <51496368+MustafaSaraoglu@users.noreply.github.com>
2021-04-28 15:38:11 +0200

    V2 can overtake and turn left.

--- src/Scripts/Load Data/load_scenario.m
+++ src/Scripts/Load Data/load_scenario.m
@@ -8,8 +8,8 @@ switch scenarioName
        %destinationPoints = [83 51 14 14  14  14 14 14 14 14];
        %maxSpeeds = [7 20 8 10 14 20 10 12 10 10];
         startingPoints =    [46 47 29 3 53 52 64 57 4 63];
-        destinationPoints = [54 59 78 27  3  27 42 25 3 47];
-        maxSpeeds = [3 5 8 7 14 20 10 12 10 10];
+        destinationPoints = [54 52 78 27  3  27 42 25 3 47];
+        maxSpeeds = [3 15 8 7 14 20 10 12 10 10];
         
         %% Complex crossroad scenario
 %         startingPoints =    [8  46 29 18 53 52 64 6 15 63];
