3f43055282dc66d834a583434f6e77dd7776ac87
Qianwei-Yang <yangqianwei713@gmail.com>
2021-06-19 00:46:42 +0200

    src\Scripts\Load Data\load_scenario.m : change the maxSpeed of vehicle 3 and vehicle 4 in lane merge scenario to meet the test standard. src\Simulink Classes\WaypointGenerator\V_WPGenerator_Purepursuit.m: comment out the code in line 85 to fix the bug of maxSpeed of vehicle 3.

--- src/Simulink Classes/WaypointGenerators/V_WPGenerator_PurePursuit.m
+++ src/Simulink Classes/WaypointGenerators/V_WPGenerator_PurePursuit.m
@@ -82,7 +82,7 @@ classdef V_WPGenerator_PurePursuit < WaypointGenerator
             
             % ISSUE 1: runs on straight road as well and becomes inf
             % ISSUE 2: sets the vehicle's maximum speed
-            obj.vehicle.dynamics.maxSpeed = sqrt(0.7*10/obj.curvature);%function of calculating maximum allowed speed on a curved road
+           % obj.vehicle.dynamics.maxSpeed = sqrt(0.7*10/obj.curvature);%function of calculating maximum allowed speed on a curved road
             
         end
         
