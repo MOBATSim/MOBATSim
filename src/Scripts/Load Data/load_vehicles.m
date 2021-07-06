18f03f005039c7f6ac11ac5caa6a0a1d0dc5e500
johannes-pi <57102504+johannes-pi@users.noreply.github.com>
2021-07-06 10:52:45 +0200

    more vehicles could be added with minor changes to model and matlab classes - adding startingPoint, destinationPoint and maxSpeeds for a vehicle is now optional

--- src/Scripts/Load Data/load_vehicles.m
+++ src/Scripts/Load Data/load_vehicles.m
@@ -1,5 +1,7 @@
 function Vehicles = load_vehicles(startingPoints, destinationPoints, maxSpeeds, map)
+    % Generation of vehicles
     
+    %% Names
     % To add a vehicle just add a new name to VehicleNames
     VehicleNames = [{'V1 - Sally Carrera'} ...
                     {'V2 - Lightning McQueen'} ...
@@ -10,26 +12,47 @@ function Vehicles = load_vehicles(startingPoints, destinationPoints, maxSpeeds, 
                     {'V7 - Fillmore'} ...
                     {'V8 - Guido'} ...
                     {'V9 - Mater'} ...
-                    {'V10 - Sheriff'}];
+                    {'V10 - Sheriff'} ...
+                    {'V11 - Test'}];
     nrVehicles = length(VehicleNames);
     
+    %% Dimensions
     % According to the Cuboid world standard vehicle dimensions
     size = [2 1.8 4.7]; % => [height, width, length] in meters
     sizes = repmat(size,nrVehicles,1);
     % To specifically change a vehicle's size:
     % sizes(vehicleId,:) = [2 1.8 10];
+    
+    %% Masses
     mass = 1800; % kilogram
     masses = repmat(mass,nrVehicles,1);
     % To specifically change a vehicle's mass:
     % masses(vehicleId) = 5000;
+    
+    %% Data links
     dataLinksV2V = ones(nrVehicles,nrVehicles);
     dataLinksV2I = ones(1,nrVehicles);
 
-
+    %% Starting points
+    % add default starting point 2 for every vehicle where no starting point is given
+    startingPoints(end+1:nrVehicles) = 2;
+    
+    %% Destination points
+    % add default destination point 1 for every vehicle where no destination point is given
+    destinationPoints(end+1:nrVehicles) = 1;
+    
+    %% Sensors
     frontSensorRange = 100.* ones(1,nrVehicles);
     AEBdistance = 25.* ones(1,nrVehicles);
+    
+    %% Dynamics
+    % add default max speed 10 for every vehicle where no max speed is given
+    maxSpeeds(end+1:nrVehicles) = 10;
+    
     minDeceleration = -9.15.* ones(1,nrVehicles); % -9.15 m/s^2 dec
 
+   
+    %% Generate vehicles
     Vehicles = [];
     for j=1:nrVehicles
         % Generate vehicle
@@ -37,7 +60,7 @@ function Vehicles = load_vehicles(startingPoints, destinationPoints, maxSpeeds, 
         maxSpeeds(j),sizes(j,:),dataLinksV2V(j,:),dataLinksV2I(j),masses(j),...
         frontSensorRange(j),AEBdistance(j),minDeceleration(j),map);
     
-        % Add to vehicle array
-        Vehicles = [Vehicles NewVehicle];
+        % Add vehicle to array
+        Vehicles = [Vehicles NewVehicle];  %#ok<AGROW>
     end
 end
