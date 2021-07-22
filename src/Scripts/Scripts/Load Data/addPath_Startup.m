7f2db13c12e0536cd3949b51aa06e25d16ab2a10
Laura Slabon <70685909+lslabon@users.noreply.github.com>
2021-07-22 08:27:50 +0200

    deleted copied files and folders

--- src/Scripts/Scripts/Load Data/addPath_Startup.m
+++
@@ -1,4 +0,0 @@
-% Add all the folders and subfolders to path (Project startup add to path doesn't work properly on other PCs)
-addpath(genpath('img'));
-addpath(genpath('src'));
-
--- src/Scripts/Scripts/run_Sim.m
+++
@@ -1,3 +0,0 @@
-% This function is to make sure that the "To Workspace" blocks store the data in the base workspace when simulation
-% is triggered from the GUI
-sim(configs.modelName);
