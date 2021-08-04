6fb570843037ac4c37e74dd660abfb8ece9a1d87
johannes-pi <57102504+johannes-pi@users.noreply.github.com>
2021-08-04 10:33:32 +0200

    bugfix in VehicleDriveMode - wrong placed negotiation prevented selecting right driving mode

--- src/Simulink Classes/VehicleDrivingMode.m
+++ src/Simulink Classes/VehicleDrivingMode.m
@@ -90,7 +90,7 @@ classdef VehicleDrivingMode < matlab.System & matlab.system.mixin.Propagates ...
             if(obj.vehicle.pathInfo.stopAt ~= 0)
                 %Output 5: Distance to stop before a crossroad
                 Dist2Stop = norm(obj.vehicle.dynamics.position - obj.vehicle.map.get_coordinates_from_waypoint(obj.vehicle.pathInfo.stopAt));
-                if ~DrivingMode == 3
+                if DrivingMode ~= 3
                     % choose approach the crossroad when not stopping
                     DrivingMode = 4;
                 end
