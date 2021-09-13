5d3b771d804ae217b04156bddbbba7f2b18647f2
Mustafa Saraoğlu <51496368+MustafaSaraoglu@users.noreply.github.com>
2021-09-14 00:01:46 +0200

    Removed addPath_Startup script because the Project startup already adds the required paths automatically.

--- addPath_Startup.m
+++
@@ -1,7 +0,0 @@
-% Add all the folders and subfolders to path (In case if the Project startup add to path doesn't work properly)
-%addpath(genpath('img'));
-%addpath(genpath('src'));
-
-if isempty(matlab.project.rootProject)
-    uiopen('\MOBATSim.prj',1);
-end
