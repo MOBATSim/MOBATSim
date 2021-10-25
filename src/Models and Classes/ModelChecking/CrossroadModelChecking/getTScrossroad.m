fa7f47c8bbe5e15f7a4a69b2f4bc38ebac6e0eab
johannes-pi <57102504+johannes-pi@users.noreply.github.com>
2021-10-25 14:47:37 +0200

    added class Transition System - transfered parallelisation function to this class - used in getTScrossroad

--- src/Models and Classes/ModelChecking/CrossroadModelChecking/getTScrossroad.m
+++ src/Models and Classes/ModelChecking/CrossroadModelChecking/getTScrossroad.m
@@ -11,18 +11,36 @@ end
 nrVeh = length(vehDirs);
 
 % Generate Transition Systems for first vehicle
-[S, Act, Tr, I, AP, L] = getTSvehicle(vehDirs(1));
+%[S, Act, Tr, I, AP, L] = getTSvehicle(vehDirs(1));
 % finished when only one vehicle
 if nrVeh == 1
     return;
 end
 % Generate Transition Systems for the rest
-for i = 2:nrVeh
+for i = 1:nrVeh
     % generate TS for vehicle
     [Sv, Actv, Trv, Iv, APv, Lv] = getTSvehicle(vehDirs(i));
     
-    [S, Act, Tr, I, AP, L] = TSSynthesis(S, Act, Tr, I, AP, L, Sv, Actv, Trv, Iv, APv, Lv);
+    % Test: TransitionSystem
+    TS(i) = TransitionSystem(Sv, Actv, Trv, Iv, APv, Lv);
+    %     TS2 = TransitionSystem(Sv, Actv, Trv, Iv, APv, Lv);
+    %     TSp = TransitionSystem.parallelize([TS1 TS2]);
+    %
+    %     S = TSp.states;
+    %     Act = TSp.actions;
+    %     Tr = TSp.transitions;
+    %     I = TSp.initialStates;
+    %     AP = TSp.atomicProps;
+    %     L = TSp.labels;
+    
 end
 
+TSp = TransitionSystem.parallelize(TS);
+S = TSp.states;
+Act = TSp.actions;
+Tr = TSp.transitions;
+I = TSp.initialStates;
+AP = TSp.atomicProps;
+L = TSp.labels;
 end
 
