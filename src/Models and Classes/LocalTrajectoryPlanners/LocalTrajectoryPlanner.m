7b317fea2d8e20c3399687830c64fbf5fbbe98f0
Mustafa Saraoğlu <51496368+MustafaSaraoglu@users.noreply.github.com>
2021-10-29 00:00:50 +0200

    Cartesian2Frenet optimized code execution time reduced.

--- src/Models and Classes/LocalTrajectoryPlanners/LocalTrajectoryPlanner.m
+++ src/Models and Classes/LocalTrajectoryPlanners/LocalTrajectoryPlanner.m
@@ -87,17 +87,12 @@ classdef LocalTrajectoryPlanner < matlab.System & handle & matlab.system.mixin.P
             %s:                     Traversed length along the reference roadline
             %d:                     Lateral offset - positive d means to the left of the reference road 
             
-            route = currentTrajectory([1,2],[1,3]).*[1 -1;1 -1];%Start- and endpoint of the current route
-            radian = currentTrajectory(3,1);%radian of the curved road, is 0 for straight road
-            Route_StartPoint = route(1,:);
-            Route_endPoint = route(2,:);
-            cclockwise = currentTrajectory(4,1);
+            route = currentTrajectory([1,2],[1,3]).*[1 -1;1 -1];%Start- and endpoint of the current route            
             
-            
-            if radian == 0%straight road
-                route_Vector = Route_endPoint-Route_StartPoint;
+            if currentTrajectory(3,1) == 0  % Straight road := radian value equals to zero
+                route_Vector = route(2,:)-route(1,:);
                 route_UnitVector = route_Vector/norm(route_Vector);
-                posVector = Vpos_C-Route_StartPoint; % Vector pointing from the route start point to the vehicle
+                posVector = Vpos_C-route(1,:); % Vector pointing from the route start point to the vehicle
                 
                 % Calculate "s" the longitudinal traversed distance
                 s = dot(posVector,route_UnitVector);% the projection of posVector on route_UnitVector
@@ -113,16 +108,22 @@ classdef LocalTrajectoryPlanner < matlab.System & handle & matlab.system.mixin.P
             else % Curved Road
                 
                 rotationCenter = currentTrajectory(3,[2 3]).*[1 -1]; % Get the rotation center
-                r = norm(Route_StartPoint-rotationCenter); % Get the radius of the rotation
-                startPointVector = Route_StartPoint-rotationCenter;% vector OP_1 in Frenet.xml
+                startPointVector = route(1,:)-rotationCenter;% Vector pointing to the start of the route from the rotation center
+                r = norm(startPointVector); % Get the radius of the rotation
                 
+                posVector = Vpos_C-rotationCenter;% the vector from rotation center pointing the vehicle
+                l = norm(posVector);
                 
-                posVector = Vpos_C-rotationCenter;% the vector from rotation center to position
-                d=(norm(posVector)-r)*cclockwise;
+                % currentTrajectory(4,1) == +1 for CounterClockwise direction 
+                d=(l-r)*currentTrajectory(4,1); % lateral displacement of the vehicle from the d=0 reference road (arc)
                               
-                angle = real(acos(dot(posVector,startPointVector)/(norm(posVector)*norm(startPointVector))));
-
-                s = angle*r;
+                angle = real(acos(dot(posVector,startPointVector)/(l*r))); % Angle between vectors
+                % One possible issue is that arccos doesn't distinguish the vector being ahead or behind, therefore when the 
+                % vehicle starts the new route, it might get a positive angle and a positive "s" value instead of a negative 
+                % one. This can be resolved using a third reference, the vector from the rotation point to the end of the 
+                % route but for the performance this is not implemented here.
+                
+                s = angle*r; % Traversed distance along the reference arc
             end
         end
         
