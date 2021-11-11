0525822b126bf8254c24bc7142d8b7e1e1ccff1a
Manuel Schirmer <manuel.schirmer@mailbox.tu-dresden.de>
2021-11-11 10:34:53 +0100

    Moved functionality to super class.

--- src/Models and Classes/LocalTrajectoryPlanners/PurePursuit_WPGenerator.m
+++ src/Models and Classes/LocalTrajectoryPlanners/PurePursuit_WPGenerator.m
@@ -8,8 +8,6 @@ classdef PurePursuit_WPGenerator < LocalTrajectoryPlanner
         
         currentPathPoints =[];  % Arrays of waypoints to follow
         laneChangingPoints =[]; % Arrays of waypoints to follow for lane changing
-        
-        laneChangeTime = 4; % Default lane-changing maneuver time duration
     end
     
     methods
@@ -108,46 +106,6 @@ classdef PurePursuit_WPGenerator < LocalTrajectoryPlanner
                     end
                 end
             end
-        end
-                                
-        function newWP_all=generateMinJerkTrajectory(obj,car,t_f,changeLane)
-            % Minimum Jerk Trajectory Generation            
-            if changeLane ==1 % To the left
-                y_f = obj.laneWidth; % Target lateral - d coordinate
-                car.pathInfo.laneId = car.pathInfo.laneId + 0.5; % Means the vehicle is in between lanes - switching to left
-            elseif changeLane ==2 % To the right
-                y_f = 0; % Target lateral - d coordinate
-                car.pathInfo.laneId = car.pathInfo.laneId - 0.5; % Means the vehicle is in between lanes - switching to right
-            end
-                        
-            % Minimun jerk trajectory function for the calculation in "d" direction (Frenet Lateral)
-            % Determining the polynomial coefficients for  lateral position, speed and acceleration
-
-            % Initial boundary conditions
-            t_i = 0; % initial time always set to zero (relatively makes no difference)
-            d_i =   [  1     t_i   t_i^2    t_i^3     t_i^4     t_i^5];
-            d_dot_i = [0     1   2*t_i  3*t_i^2   4*t_i^3   5*t_i^4];
-            d_ddot_i = [0     0     2    6*t_i  12*t_i^2  20*t_i^3];
-            
-            d_ti = [car.pathInfo.d; 0; 0]; % Initial lateral position, speed, acceleration
-            
-            % Final boundary conditions
-            d_f =   [  1     t_f   t_f^2    t_f^3     t_f^4     t_f^5];
-            d_dot_f = [0     1   2*t_f  3*t_f^2   4*t_f^3   5*t_f^4];
-            d_ddot_f = [0     0     2    6*t_f  12*t_f^2  20*t_f^3];
-            
-            d_tf = [y_f; 0; 0]; % Final lateral position, speed, acceleration
-            
-            A = [d_i;d_dot_i;d_ddot_i;d_f;d_dot_f;d_ddot_f];
-            B = [d_ti;d_tf];
-            % Solve for the coefficients using 6 linear equations with 6 boundary conditions at t = ti and t = tf
-            a = linsolve(A,B);
-                        
-            tP = 0:0.2:t_f; % To calculate the lane changing path points for the trajectory with 0.2 s intervals
-            newWP_s=car.dynamics.speed*tP; % Target longitudinal - "s" coordinates of the path points
-            newWP_d=a(1)+a(2)*tP+a(3)*tP.^2+a(4)*tP.^3+a(5)*tP.^4+a(6)*tP.^5; % "d" coordinates corresponding to the trajectory
-            newWP_all =  [newWP_s' newWP_d']; % create a set of path points
-            obj.ref_d = a(1)+a(2)*t_f+a(3)*t_f^2+a(4)*t_f^3+a(5)*t_f^4+a(6)*t_f^5; % reference "d" value by the end of the lane changing maneuver
         end
         
         function currentPathPoints = generatePathFollowingWaypoints(~,Vpos,nextPoints,K)
